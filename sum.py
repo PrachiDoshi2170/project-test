62a9a1951c238fa4a34abbc42749c1d44708d2b4
Prachi Doshi <prachidoshi721@gmail.com>
2024-10-08 14:05:47 -0700

    Update sum.py

--- sum.py
+++ sum.py
@@ -3,6 +3,7 @@ b = int(input("Enter another number: "))  # Convert input to int
 
 if a > 0 and b > 0:  # Use 'and' for logical AND
     c = a + b
+    c = a - b
     print(f"The sum of {a} and {b} is {c}")
 else:
     print("Enter positive numbers")
