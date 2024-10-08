29e6c9d91a88dbe229171e193ec9c9402f3387ae
Prachi Doshi <prachidoshi721@gmail.com>
2024-10-08 01:35:56 -0700

    Create multiplication.py

---
+++ multiplication.py
@@ -0,0 +1,6 @@
+
+a = int(input("Enter the number: "))  # Convert input to int
+b = int(input("Enter another number: "))  # Convert input to int
+c = a * b
+print(f"The sum of {a} and {b} is {c}")
+
